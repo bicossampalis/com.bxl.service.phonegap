7e48161644ac21239fd63eedc811b9e9a4937b03
bicossampalis <beaucoupfish25@hotmail.com>
2017-03-28 18:34:39 +0300

    1.80

--- src/android/BXLService.java
+++ src/android/BXLService.java
@@ -2,8 +2,8 @@ package com.bxl.service.phonegap;
 
 import jpos.JposException;
 
-//import jpos.POSPrinter;
-import bixolon_printer.EpsPOSPrinter;
+import jpos.POSPrinter;
+//import bixolon_printer.EpsPOSPrinter;
 
 import jpos.config.JposEntry;
 
@@ -96,8 +96,8 @@ public class BXLService extends CordovaPlugin {
     private final String METHOD_TRANSACTION_PRINT = "transactionPrint";
     private final String METHOD_ADD_ENTRY = "addEntry";
 
-    //private static POSPrinter posPrinter;
-	private static EpsPOSPrinter posPrinter;
+    private static POSPrinter posPrinter;
+	//private static EpsPOSPrinter posPrinter;
 
     BluetoothAdapter mBTAdapter;
     Set<BluetoothDevice> pairedDevices;
@@ -113,8 +113,8 @@ public class BXLService extends CordovaPlugin {
 			bxlConfigLoader = new EpsBXLConfigLoader(context);
 		}
 
-		//posPrinter = new POSPrinter(context);
-		posPrinter = new EpsPOSPrinter(context);
+		posPrinter = new POSPrinter(context);
+		//posPrinter = new EpsPOSPrinter(context);
     }
 
     @Override
@@ -253,10 +253,10 @@ public class BXLService extends CordovaPlugin {
 				if (printFunction.equals(METHOD_PRINT_NORMAL)) {
 					posPrinter.printNormal(args.getInt(5), args.getString(6));
 				} 
-				else if (printFunction.equals(METHOD_PRINT_LINE)){
-					posPrinter.printLine("", args.getInt(7), args.getInt(8), args.getInt(9), args.getInt(10), args.getInt(11));
-					posPrinter.printNormal(args.getInt(5), args.getString(6));
-				} 
+				// else if (printFunction.equals(METHOD_PRINT_LINE)){
+					// posPrinter.printLine("", args.getInt(7), args.getInt(8), args.getInt(9), args.getInt(10), args.getInt(11));
+					// posPrinter.printNormal(args.getInt(5), args.getString(6));
+				// } 
 				else if (printFunction.equals(METHOD_PRINT_BITMAP_WITH_BASE64)) {
 				
 					String base64EncodedData= args.getString(6);
